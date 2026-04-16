d36d7619aee7ad7b4a8ab16b923a9a5a11af1bfa
Your Name <your.email@gmail.com>
2026-04-15 20:25:58 -0700

    Set custom page title and favicon

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Ryan Bar-Zion Portfolio</title>
+      <meta name="description" content="AI & Research Tools by Ryan Bar-Zion" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
